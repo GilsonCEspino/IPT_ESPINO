458b5220553d83ffd7bec8a62b0b1fb1957f6f74
BSIT3E_ESPINO <189708884+GilsonCEspino@users.noreply.github.com>
2024-11-29 20:43:42 +0800

    Delete docs directory

--- docs/JS/script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scrolling to sections (if the links are added)
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
